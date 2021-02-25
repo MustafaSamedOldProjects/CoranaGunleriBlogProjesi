842fb935fdf35c67b22c9133ff1e806206963147
MustafaSamedYeyin <mustafasamedyeyin@gmail.com>
2021-02-25 16:57:02 +0300

    AutoMapper-daha tamamlanamadı-.

--- Blog/Map/Mapper.cs
+++ Blog/Map/Mapper.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using AutoMapper;
+using DTOs.Concrete;
+using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Blog.Map
 {
-    public class Mapper
+    public class Mapper :Profile
     {
+        public Mapper()
+        {
+            CreateMap<SignInIdentityDto, AppUser>();
+            CreateMap<SignUpIdentityDto, AppUser>();
+            CreateMap<AppUser, SignInIdentityDto>();
+            CreateMap<AppUser, SignUpIdentityDto>();
+        }
     }
 }
